75a736f517e23f3bc375938d972802f041f1b8cf
iwasakisinnya <rwkn.pta1078@gmail.com>
2017-11-05 21:32:50 +0900

    [Fadd] (C) 値参照遊び
    自分のコードのエラー直しのため。
    現状この状態で実行させると計算がうまくいきません。

---
+++ 04_value.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void ALgo(double a,int b){
+  double pi = 3.14;
+
+  a = pi*b ;
+}
+
+int main (void){
+  int i;
+  double alpha = 0;
+
+  for (i=0;i<4;i++){
+    ALgo(alpha,i);
+    printf("pi*%d = %lf \n",i,alpha);
+  }
+
+  return 0;
+}
