9fa08d179891e3d4be201abce16288a59326fe54
iwasakisinnya <rwkn.pta1078@gmail.com>
2017-11-05 21:45:57 +0900

    [Change Add] (C) 関数参照の方法
    先ほどの変更点で、Alphaを明示的に変更(マイナーチェンジ)

--- 04_value.c
+++ 04_value.c
@@ -7,7 +7,9 @@ void ALgo(double *a,int b){
 
 int main (void){
   int i;
-  double alpha = 0;
+  double alpha = 0.;
+  // add (In order to explicitly "alpha")
+  printf("alpha is %lf",alpha);
 
   for (i=0;i<4;i++){
     ALgo(&alpha,i);
